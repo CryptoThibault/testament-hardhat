298bf6bcc3578a8821d8eed700d6a68b8dfb4021
CryptoThibault <thibault.chalaoux@gmail.com>
2021-06-07 16:52:13 +0200

    start test

--- contracts/Testament.sol
+++ contracts/Testament.sol
@@ -8,19 +8,40 @@ contract Testament {
   address _doctor;
   bool _died;
 
+  event Donation(address indexed receiver, uint amount);
+  event SetDoctor(address lastDoctor, address newDoctor);
+  event SetDied(address owner, uint timestamp);
+  event Withdrew(address indexed sender, uint amount);
+
   constructor(address doctor_) {
     _owner = msg.sender;
     _doctor = doctor_;
+    emit SetDoctor(address(0), doctor_);
+  }
+  
+  modifier ownerOnly {
+    require (msg.sender == _owner, "Testament: reserved to testament owner");
+    _;
   }
 
-  function donate(address to) public payable returns (bool) {
-    require(msg.sender == _owner, "Testament: reserved to testament owner");
+  function donate(address to) public payable ownerOnly returns (bool) {
+    require (to != msg.sender, "Testament: cannot donate to yourself");
     _balances[to] += msg.value;
+    emit Donation(to, msg.value);
+    return true;
+  }
+  function setDoctor(address doctor_) public ownerOnly returns (bool) {
+    require(doctor_ != _owner, "Testament: cannot set yourself as doctor");
+    emit SetDoctor(_doctor, doctor_);
+    _doctor = doctor_;
     return true;
   }
 
   function setDied() public returns (bool) {
-    require(msg.sender == _doctor, "Testament: reserved to testament doctor")
+    require(msg.sender == _doctor, "Testament: reserved to testament doctor");
+    _died = true;
+    emit SetDied(_owner, block.timestamp);
+    return true;
   }
 
   function withdraw() public returns (bool) {
@@ -28,11 +49,18 @@ contract Testament {
     uint amount = _balances[msg.sender];
     _balances[msg.sender] = 0;
     payable(msg.sender).transfer(amount);
+    emit Withdrew(msg.sender, amount);
     return true;
   }
 
+  function balance() public view returns (uint) {
+    return _balances[msg.sender];
+  }
   function owner() public view returns (address) {
     return _owner;
+  }
+  function doctor() public view ownerOnly returns (address) {
+    return _doctor;
   }
   function died() public view returns (bool) {
     return _died;
--- test/Testament-test.js
+++ test/Testament-test.js
@@ -1,3 +1,55 @@
-/* eslint-disable no-unused-vars */
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
+
+describe('Testament', async function () {
+  let TESTAMENT, testament, owner, doctor, alice, bob;
+  const GIVE_VALUE = ethers.utils.parseEther('0.001');
+  beforeEach(async function () {
+    ;[owner, doctor, alice, bob] = await ethers.getSigners();
+    TESTAMENT = await ethers.getContractFactory('Testament');
+    testament = await TESTAMENT.connect(owner).deploy(doctor.address);
+    await testament.deployed();
+  });
+  describe('Deployment', async function () {
+    it('Should be the good owner', async function () {
+      expect(await testament.owner()).to.equal(owner.address);
+    });
+    it('Should be the good doctor', async function () {
+      expect(await testament.connect(owner).doctor()).to.equal(doctor.address);
+    });
+    it('Should emits event SetDoctor in constructor', async function () {
+      const receipt = await testament.deployTransaction.wait();
+      expect(receipt.transactionHash)
+        .to.emit(testament, 'SetDoctor')
+        .withArgs(ethers.constants.AddressZero, doctor.address);
+    });
+  });
+  describe('Donate', async function () {
+    let DONATE;
+    beforeEach(async function () {
+      DONATE = await testament.connect(owner).donate(alice.address, { value: GIVE_VALUE });
+    });
+    it('Should change balances of owner and contract', async function () {
+      expect(DONATE).to.changeEtherBalances([owner, testament], [-GIVE_VALUE, GIVE_VALUE]);
+    });
+    it('Should change user balance inside the contract', async function () {
+      expect(await testament.connect(alice).balance()).to.equal(GIVE_VALUE);
+    });
+    it('Should emit events Donation', async function () {
+      expect(DONATE).to.emit(testament, 'Donation').withArgs(alice.address, GIVE_VALUE);
+    });
+    it('Should revert call if receiver is also owner', async function () {
+      await expect(testament.connect(owner).donate(owner.address, { value: GIVE_VALUE }))
+        .to.revertedWith('Testament: cannot donate to yourself');
+    });
+    it('Should revert call if sender is not owner', async function () {
+      await expect(testament.connect(alice).donate(bob.address, { value: GIVE_VALUE }))
+        .to.revertedWith('Testament: reserved to testament owner');
+    });
+  });
+  describe('Doctor', async function () {
+    it('Should set bob as a new doctor');
+    it('Should revert call if new doctor is owner');
+    it('Should emit events setDoctor');
+  });
+});
